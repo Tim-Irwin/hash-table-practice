8fc3713db2a51f2fb1c1bfec162da49049a07afd
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-10-27 16:13:49 -0700

    anagrams

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,5 +1,37 @@
 function anagrams(str1, str2) {
   // Your code here
+  if(str1.length !== str2.length) return false;
+  // let arr = str1.split("");
+  // arr.forEach(item =>{
+  //   if (!(str2.includes(item)) ){
+  //     return false;
+  //   }
+  // })
+  // return true;
+  let obj1 = {};
+  for(let i = 0; i < str1.length; i++){
+    if (!obj1[str1[i]]){
+      obj1[str1[i]] = 0;
+    }
+    obj1[str1[i]]++;
+  }
+  let obj2 = {};
+  for(let i = 0; i < str2.length; i++){
+    if (!obj2[str2[i]]){
+      obj2[str2[i]] = 0;
+    }
+    obj2[str2[i]]++;
+  }
+  for(let key in obj1){
+    // if(!(key instanceof obj2))
+    if(!(obj2.hasOwnProperty(key))){
+      return false;
+    }
+    if(obj1[key] !== obj2[key]){
+      return false;
+    }
+  }
+  return true;
 }
 
 
