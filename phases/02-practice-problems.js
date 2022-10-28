4af3be4d8fdce087c60aef94f544819458adf0fa
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-10-28 11:11:18 -0700

    02-practice-problems

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -36,32 +36,87 @@ function anagrams(str1, str2) {
 
 
 function commonElements(arr1, arr2) {
-  const res = [];
-
-  arr1.forEach(num1 => {
-    arr2.forEach(num2 => {
-    if(num1 = num2){
-      res.push(num1);
-      }    
-    })
-  })
+  let res = [];
+  let obj1 = {};
+  for(let i = 0; i < arr1.length; i++){
+    if (!obj1[arr1[i]]){
+      obj1[arr1[i]] = 0;
+    }
+    obj1[arr1[i]]++;
+  }
+  for (let i = 0; i < arr2.length; i++){
+    if (obj1.hasOwnProperty(arr2[i])){
+      res.push(arr2[i]);
+    }
+  }
   return res;
 }
 
 
 function duplicate(arr) {
   // Your code here
+  let obj = {};
+  for (let i = 0; i < arr.length; i++){
+    if (!obj.hasOwnProperty(arr[i])){
+      obj[arr[i]] = 1;
+    }else{
+      obj[arr[i]]++;
+      if (obj[arr[i]] === 2){
+        return arr[i];
+      }
+    }
+  }
+  return null;
 }
 
 
 function twoSum(nums, target) {
   // Your code here
-}
+  debugger;
+  let set = new Set();
+  for (let i = 0; i < nums.length; i++){
+    if (!set.size){
+      set.add(nums[i])
+    }else{
+      if (set.has(target - nums[i])){
+        return true;
+      }else{
+        set.add(nums[i]);
+      }
+    }
 
+  }
+  return false;
+}
+// console.log(twoSum([2, 7, 11, 15], 22));
+// console.log(twoSum([3, 4, 5], 6));
 
 function wordPattern(pattern, strings) {
   // Your code here
+  let obj = {};
+  let set = new Set();
+  let count = 0;
+  for (let i = 0; i < strings.length; i++){
+    set.add(strings[i]);
+  }
+  for (let i = 0; i < pattern.length; i++){
+    if (!obj.hasOwnProperty(pattern[i])){
+      obj[pattern[i]] = [];
+      count++;
+    }
+    obj[pattern[i]].push(i);
+  }
+  if(count !== set.size) return false;
+  for (let key in obj){
+    let indices = obj[key];
+    let string = strings[indices[0]];
+    for (let i = 1; i < indices.length; i++){
+      if (strings[indices[i]] !== string){
+        return false;
+      }
+    }
+  }
+  return true;
 }
-
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
